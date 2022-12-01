a9689a8483fa8c960e632695652b6cd0aaba00b6
Adriana <adrianafsamaral@gmail.com>
2022-12-01 16:49:03 -0300

    alterações nas cores

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,7 +9,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "green" : "blue"}
+          color={showAdd ? "red" : "green"}
           text={showAdd ? "Fechar" : "Adicionar"}
           onClick={onAdd}
         />
